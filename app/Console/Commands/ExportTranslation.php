295f559781acc5ddf4f8a3bd432e7d40af44882c
Neodc <noe@lal.be>
2015-06-01 17:29:25 +0200

    Importing previous work in this new project

---
+++ app/Console/Commands/ExportTranslation.php
@@ -0,0 +1,147 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Classes\LaravelExcelWorksheet;
+use Symfony\Component\Console\Input\InputOption;
+
+class ExportTranslation extends Command
+{
+	protected $name = 'bepark:translation:export';
+
+	protected $description = 'Export the translation to an excel';
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$dir = rtrim($this->option('dir'), '/');
+		$out = $this->option('out');
+		$locals = $this->option('local');
+		$langs = $this->option('lang');
+		$ref = $this->option('ref');
+
+		if ($dir == null)
+		{
+			$this->error('The dir parameter is mandatory.');
+
+			return 1;
+		}
+
+		if (empty($langs))
+		{
+			$this->error('The lang parameter is mandatory.');
+
+			return 1;
+		}
+
+		if (empty($locals)) // if no local given we get all the locals of the reference.
+		{
+			$locals = array_map(function ($v)
+			{
+				return pathinfo($v, PATHINFO_FILENAME);
+			}, glob($dir . '/' . $ref . '/*'));
+		}
+
+		$data = [];
+
+		foreach ($locals as $local)
+		{
+			$data[ $local ] = $this->_getData($dir, $local, $langs, $ref);
+		}
+
+		$this->info('Create xls to ' . pathinfo($out, PATHINFO_BASENAME));
+
+		\Excel::create(pathinfo($out, PATHINFO_BASENAME), function ($excel) use ($data)
+		{
+
+			$excel->sheet('__info_',
+				function ($sheet)
+				{
+					$sheet->fromArray([
+						[
+							'Don\'t touch key and ref column'
+						]
+					],
+						null,
+						'A1',
+						false,
+						false);
+				});
+
+			foreach ($data as $name => $local)
+			{
+				$excel->sheet($name,
+					function (LaravelExcelWorksheet $sheet) use ($local)
+					{
+						$sheet->fromArray($local);
+					});
+			}
+		})->store('xls', pathinfo($out, PATHINFO_DIRNAME));
+	}
+
+	protected function getOptions()
+	{
+		return array(
+			array('dir', 'd', InputOption::VALUE_REQUIRED, 'the path to the directory to analyse'),
+			array('local', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'The translations file to use (if not present, all)'),
+			array('lang', 'l', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'The languages to analyse'),
+			array('out', 'o', InputOption::VALUE_REQUIRED, 'The output file name (without extention)', 'local'),
+			array('ref', null, InputOption::VALUE_REQUIRED, 'The lang to use as reference', 'ref'),
+		);
+	}
+
+	protected function _getData($dir, $local, $langs, $ref)
+	{
+		$data = [];
+
+		foreach ($this->_include($dir, $ref, $local) as $k => $v)
+		{
+			$data[ $k ] = $this->_emptyLine($k, $v, $langs);
+		}
+
+		foreach ($langs as $lang)
+		{
+			foreach ($this->_include($dir, $lang, $local) as $k => $v)
+			{
+				if (!isset($data[ $k ]))
+				{
+					$data[ $k ] = $this->_emptyLine($k, 'N/A', $langs);
+				}
+
+				$data[ $k ][ $lang ] = $v;
+			}
+		}
+
+		return $data;
+	}
+
+	protected function _include($dir, $lang, $local)
+	{
+		try
+		{
+			return array_dot(include $dir . '/' . $lang . '/' . $local . '.php');
+		}
+		catch (\Exception $e)
+		{
+			return [];
+		}
+	}
+
+	protected function _emptyLine($key, $ref, $langs)
+	{
+		$t = [
+			'key' => $key,
+			'ref' => $ref,
+		];
+
+		foreach ($langs as $lang)
+		{
+			$t[ $lang ] = null;
+		}
+
+		return $t;
+	}
+}
---
+++ app/Console/Commands/ImportTranslation.php
@@ -0,0 +1,128 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Collections\SheetCollection;
+use Symfony\Component\Console\Input\InputOption;
+
+class ImportTranslation extends Command
+{
+	protected $name = 'bepark:translation:import';
+
+	protected $description = 'Import the translation from an excel to php files';
+
+	public function fire()
+	{
+		$file = $this->option('file');
+		$out = rtrim($this->option('out'), '/');
+		$locals = $this->option('local');
+		$langs = $this->option('lang');
+
+		if ($file == null)
+		{
+			$this->error('The file parameter is mandatory.');
+
+			return 1;
+		}
+
+		if ($out == null)
+		{
+			$this->error('The out parameter is mandatory.');
+
+			return 1;
+		}
+
+		if (empty($langs))
+		{
+			$this->error('The language parameter is mandatory.');
+
+			return 1;
+		}
+
+		if (empty($locals))
+		{
+			/* @var $data \Maatwebsite\Excel\Readers\LaravelExcelReader */
+			$data = \Excel::load($file);
+
+			// XXX avoid range error like [PHPExcel_Exception] Row 2 is out of range (2 - 1)
+			$sheetNumber = $data->getExcel()->getSheetCount();
+			$data->setSelectedSheetIndices(range(1, $sheetNumber));
+
+			$data = $data->get();
+		}
+		else
+		{
+			$data = \Excel::selectSheets($locals)->load($file)->get();
+		}
+
+		if ($data instanceof SheetCollection)
+		{
+			$t = [];
+			foreach ($data as $sheet)
+			{
+				$t[ $sheet->getTitle() ] = $sheet->toArray();
+			}
+			$data = $t;
+		}
+		else
+		{
+			$data = [$data->getTitle() => $data->toArray()];
+		}
+
+		$i = 0;
+		foreach ($data as $local => $sheet)
+		{
+			if ($local == '__info_')
+			{
+				continue;
+			}
+
+			$this->output->write('processing ' . $local);
+
+			$export = [];
+			foreach ($sheet as $row)
+			{
+				foreach ($langs as $lang)
+				{
+					$export[ $lang ][ $row[ 'key' ] ] = $row[ $lang ];
+				}
+			}
+
+			foreach ($export as $lang => $e)
+			{
+				// export it
+				$this->_export($out, $lang, $local, $e);
+			}
+			$this->info(' Done');
+			$i++;
+		}
+
+		$this->info($i . ' local(s) processed');
+	}
+
+	protected function getOptions()
+	{
+		return [
+			['file', 'f', InputOption::VALUE_REQUIRED, 'the path to the file to analyse'],
+			['local', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'The translations to import (if not present, all)'],
+			['lang', 'l', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'The languages to import'],
+			['out', 'o', InputOption::VALUE_REQUIRED, 'The output directory name'],
+		];
+	}
+
+	protected function _export($dir, $lang, $local, $data)
+	{
+		// assure order of key => easier to compare on diff
+		ksort($data);
+
+		$path = $dir . '/' . $lang . '/' . $local . '.php';
+		@mkdir(pathinfo($path, PATHINFO_DIRNAME));
+
+		$array = array();
+		foreach ($data as $key => $value)
+		{ // convert dot notation in real array
+			array_set($array, $key, $value);
+		}
+
+		file_put_contents($path, '<?php' . PHP_EOL . PHP_EOL . 'return ' . var_export($array, true) . ';' . PHP_EOL);
+	}
+}
